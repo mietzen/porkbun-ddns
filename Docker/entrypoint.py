fedd88eb63383abdcf3c1704167b49e6236ec827
Nils Stein <31704359+mietzen@users.noreply.github.com>
2023-06-17 10:54:42 +0200

    Initialize vars first

--- Docker/entrypoint.py
+++ Docker/entrypoint.py
@@ -3,6 +3,25 @@ import sys
 from time import sleep
 from porkbun_ddns import PorkbunDDNS, cli
 
+sleep_time = int(os.getenv('SLEEP', 300))
+domain = os.getenv('DOMAIN', None)
+
+public_ips = None
+if os.getenv('PUBLIC_IPS', None):
+    public_ips = [x.strip() for x in os.getenv('PUBLIC_IPS', None).split(',')]
+fritzbox = os.getenv('FRITZBOX', None)
+
+config = {
+    'secretapikey': os.getenv('SECRETAPIKEY'),
+    'apikey': os.getenv('APIKEY')
+}
+
+ipv4 = ipv6 = True
+if os.getenv('IPV4_ONLY', 'False').lower() in ('true', '1', 't'):
+    ipv6 = False
+if os.getenv('IPV6_ONLY', 'False').lower() in ('true', '1', 't'):
+    ipv4 = False
+
 if os.getenv('INTEGRATION_TEST'):
     print('\n------------------------------------')
     print('INTEGRATION TEST! Printing help menu')
@@ -16,29 +35,11 @@ if os.getenv('INTEGRATION_TEST'):
             print('\n------------------------------------')
             print('Sleeping...')
             print('------------------------------------\n')
-            sleep(300)
+            sleep(sleep_time)
 
 if not all([os.getenv('DOMAIN'), os.getenv('SECRETAPIKEY'), os.getenv('APIKEY')]):
     print('Please set DOMAIN, SECRETAPIKEY and APIKEY')
     sys.exit(1)
-
-domain = os.getenv('DOMAIN', None)
-public_ips = None
-if os.getenv('PUBLIC_IPS', None):
-    public_ips = [x.strip() for x in os.getenv('PUBLIC_IPS', None).split(',')]
-fritzbox = os.getenv('FRITZBOX', None)
-sleep_time = int(os.getenv('SLEEP', 300))
-
-config = {
-    'secretapikey': os.getenv('SECRETAPIKEY'),
-    'apikey': os.getenv('APIKEY')
-}
-
-ipv4 = ipv6 = True
-if os.getenv('IPV4_ONLY', 'False').lower() in ('true', '1', 't'):
-    ipv6 = False
-if os.getenv('IPV6_ONLY', 'False').lower() in ('true', '1', 't'):
-    ipv4 = False
 
 if not any([ipv4, ipv6]):
     print('You can not set both IPV4_ONLY and IPV6_ONLY to TRUE')
