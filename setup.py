21d641861176e9bdb7b52b957558e8c1c60b6832
Jan Philipp Berg <git.7ksst@aleeas.com>
2024-03-31 13:49:56 +0200

    Add 'xdg' to dependencies
    This provides the default config-file-location for config.py

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
     ],
     keywords="porkbun ddns",
     packages=["porkbun_ddns"],
+    install_requires=["xdg~=5.1"],
     entry_points={
         'console_scripts': ['porkbun-ddns=porkbun_ddns.cli:main']
     },
